f059b1c81abf1e14b9eca02b6be13da507e0fa86
Ritesh-Sivanathan <ritesh.sivanathan@gmail.com>
2024-12-27 13:56:39 -0500

    added flag to run as super user

--- run.c
+++ run.c
@@ -1,26 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 #define COMMAND_LENGTH 256
 #define ARGUMENTS_LENGTH 256
 
 int main(int argc, char *argv[]) {
 
-    // argv[1] is the filename passed in
+    // NO FLAGS: argv[1] = filename
+    // YES FLAGS: argv[2] = filename
     // anything after will just be appended to the execution string
 
     if (argc < 2) {
-        fprintf(stderr, "easy_c_run usage: %s <source_file>", argv[0]);
+        fprintf(stderr, "easy_c_run usage: %s <flags> <source_file>\n", argv[0]);
         return 1;
     }
 
     char source_filename[COMMAND_LENGTH];
-    strncpy(source_filename, argv[1], COMMAND_LENGTH - 1);
+    int x = 1; // index of source_filename in the command. default is 1 (no flags)
+
+    if (argc > 2) {
+	    if ( (strcmp (argv[1], "--sudo") ) == 0 ) {
+	    	x = 2;
+       	    }
+    }
+
+    strncpy(source_filename, argv[x], COMMAND_LENGTH - 1);
     source_filename[COMMAND_LENGTH - 1] = '\0';
 
+    char optionalArguments[ARGUMENTS_LENGTH] = " ";
+    char runAsSudo[5] = "";
+
+
+   if (argc > 1) {
+
+	if(strcmp(argv[1], "--sudo")==0) {
+		for (int i=3;i<argc;i++) { // becomes i=3 since the first 2 elements are filename and --sudo flag
+			strcat(optionalArguments, argv[i]);
+			strcat(optionalArguments, " ");
+		}
+
+		strcat(runAsSudo, "sudo");
+
+	} else {
+		for (int i=2;i<argc;i++) {
+			strcat(optionalArguments, argv[i]);
+			strcat(optionalArguments, " ");
+		}
+	}
+
+   }
+
+
     char compile_command[COMMAND_LENGTH];
-    snprintf(compile_command, COMMAND_LENGTH, "gcc %s.c -o %s", source_filename, source_filename);
+    snprintf(compile_command, COMMAND_LENGTH, "%s gcc %s.c -o %s", runAsSudo, source_filename, source_filename);
 
     int return_code_c = system(compile_command);
 
@@ -30,14 +64,6 @@ int main(int argc, char *argv[]) {
     }
 
     char execute_command[COMMAND_LENGTH];
-    char optionalArguments[ARGUMENTS_LENGTH] = " ";
-
-   if (argc > 1) {
-	for (int i=2;i<argc;i++) {
-		strcat(optionalArguments, argv[i]);
-		strcat(optionalArguments, " ");
-	}
-   }
 
     snprintf(execute_command, COMMAND_LENGTH, "./%s", source_filename); // need to add flags here
     strcat(execute_command, optionalArguments);
